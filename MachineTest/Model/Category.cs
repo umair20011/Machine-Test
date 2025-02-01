6ccd675c17f07ebc529f83fd08e429a2944d38a5
Danish <danishmaniyar2110@gmail.com>
2025-02-01 22:01:31 +0530

    Added model for Product and Category

---
+++ MachineTest/Model/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductManagement.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public string CategoryName { get; set; }
+
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+}
---
+++ MachineTest/Model/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductManagement.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        public string ProductName { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
